ad0753dc8e7bf3c30117f40f8031ad366d723702
mirynth <michellewong.nma@gmail.com>
2014-04-07 18:39:02 -0700

    Add : Process function
    Process processes the input and breaks it down into its components,
    deciding which function to call. This is to allow a whole string of
    input instead of having to type "enter" after typing "set" and "get"

--- internship/main.cpp
+++ internship/main.cpp
@@ -2,12 +2,57 @@
 
 using namespace std;
 
+void Process(istream input); // fx that reads input and decides which function to call
+
+template<class T>
+void get(T propertyname) // prints out current value
+{
+    cout << propertyname;
+}
+
+template<class T>
+void set(T propertyname, T newvalue)
+{
+
+}
+
 int main()
 {
-    string command;
+    string input;
 
-    cout << " > "
-    cout << "Hello World!" << endl;
+    cout << "#--------Command Menu--------#" << endl;
+    cout << "SET : Change value" << endl;
+    cout << "GET : Output value" << endl;
+
+    cin >> input;
+
+    Process(input);
+
     return 0;
 }
 
+template<class T>
+void Process(istream input)
+{
+    string command;
+    char assign; // equals sign
+    T propertyname,newvalue; // two variables of same typename
+
+    in >> command >> propertyname >> assign >> newvalue;
+
+    switch(command)
+    {
+        case 'SET':
+            set(propertyname, newvalue);
+            break;
+        case 'GET':
+            get(propertyname);
+            break;
+        case 'GET *':
+            printall(propertyname);
+            break;
+        default:
+            cout << "command does not exist" << endl;
+            break;
+    }
+}
