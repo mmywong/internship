a76a6111b47dfbc538860a9b496b3bf798a5a0dd
mirynth <michellewong.nma@gmail.com>
2014-04-08 14:44:42 -0700

    update

--- internship/main.cpp
+++ internship/main.cpp
@@ -1,9 +1,76 @@
 #include <iostream>
+#include <stdio.h>
+#include <string.h>
 
 using namespace std;
 
- void Process(istream input); // fx that reads input and decides which function to call
+void Process(string input);
 
+int main()
+{
+    string input;
+
+    cout << "#--------Command Menu--------#" << endl;
+    cout << "SET : Change value" << endl;
+    cout << "GET : Output value" << endl;
+    cout << "GET* : Output list of values" << endl;
+
+    string command;
+    char assign; // equals sign
+    T propertyname,newvalue; // two variables of same typename
+
+    input >> command >> propertyname >> assign >> newvalue;
+
+
+
+    return 0;
+}
+
+void Process(string input)
+{
+    char* command, propertyname, newvalue, type;
+    char** A = new char *[3];
+
+    for (int i=0; i < 3; i++)
+        A[i] = new char [50];
+
+    int i = 0;
+
+    //chops the string into parts
+    while(A[i])
+    {
+        A[i] = strtok(input," =");
+        i++;
+    }
+    command = A[0]; //collects to read GET/SET/GET*
+    propertyname = A[1]; //collects to read propertyname
+
+    switch(toupper(command))
+    {
+        case 'ADD':
+            type = A[2];
+            break;
+        case 'SET':
+            newvalue = A[2];
+            set(propertyname, newvalue);
+            break;
+        case 'GET':
+            get(propertyname);
+            break;
+        case 'GET*':
+            printall(propertyname);
+            break;
+        default:
+            cout << "command does not exist" << endl;
+            break;
+    }
+}
+
+
+
+
+
+/*
  template<class T>
  void set(T propertyname, T newvalue)
  {
@@ -27,43 +94,3 @@ using namespace std;
      }
  }
 
-  int main()
-  {
-     istream input;
-
-     cout << "#--------Command Menu--------#" << endl;
-     cout << "SET : Change value" << endl;
-     cout << "GET : Output value" << endl << endl;
-     cout << ">";
-//     cin >> input; // check this?
-     Process(input);
-
-     cout << "Hello World!" << endl;
-     return 0;
- }
-
- template<class T>
- void Process(istream input)
- {
-     string command;
-     char assign; // equals sign
-     T propertyname,newvalue; // two variables of same typename
-
-     input >> command >> propertyname >> assign >> newvalue;
-
-     switch(command)
-     {
-         case 'SET':
-             set(propertyname, newvalue);
-             break;
-         case 'GET':
-             get(propertyname);
-             break;
-         case 'GET *':
-             printall(propertyname);
-             break;
-         default:
-             cout << "command does not exist" << endl;
-             break;
-     }
- }
