524784dbeaaf5976ce84f263f8388d423419ae08
mirynth <michellewong.nma@gmail.com>
2014-05-23 02:07:09 -0700

    Add : get and getall functions

--- internship5/internship5/main.cpp
+++ internship5/internship5/main.cpp
@@ -13,6 +13,8 @@ using namespace std;
 int whattype(char* newvalue); //detect int/char/string
 void Process(list<data> &database, char* input); //take apart input into tokens
 void set(list<data>& database, char* propertyname, char* newvalue);
+void get(list<data> database, char* propertyname);
+void getall(list<data> database);
 
 int main()
 {
@@ -21,8 +23,14 @@ int main()
     newdata.type = 1;
     newdata.value = "3";
 
+    data newdata2;
+    newdata2.propertyname = "miso";
+    newdata2.type = 2;
+    newdata2.value = "spicy";
+
     list<data> database;
     database.InsertHead(newdata);
+    database.Append(newdata2);
     cout << "current database: " << endl;
     database.Print();
 
@@ -69,9 +77,9 @@ void Process(list<data>& database, char* input)
     if((FindStr("SET", A[0], 0) != -1) || (FindStr("set", A[0],0) != -1)) // "SET" is found in command
         set(database, A[1], A[2]);
     else if((FindStr("GET*", A[0], 0) != -1) || (FindStr("get*", A[0], 0)!= -1))
-        cout << "getall();" << endl;
+        getall(database);
     else if((FindStr("GET", A[0], 0) != -1) || (FindStr("get", A[0], 0) != -1))
-        cout << "get();" << endl;
+        get(database, A[1]);
     else
         cout << "error : command does not exist" << endl;
 //    delete[] in;
@@ -93,30 +101,30 @@ void set(list<data> &database, char* propertyname, char* newvalue)
     //case 2: name exists. search for it and set its value
     else if(database.Search(newdata) != -1) // found
     {
-        Iterator<data> positionfound = database.Ithnode(database.Search(newdata));
+        Iterator<data> searchptr = database.Ithnode(database.Search(newdata));
         //case : input type is incompatible (e.g. int set to string)
-        if(newdata.type != (*positionfound).type)
+        if(newdata.type != (*searchptr).type)
             cout << "error : input value is incompatible" << endl;
         else
         {
 
             /***DEBUG***
-            cout << "what iterator(of search result) is pointing at : " << *positionfound << endl;
+            cout << "what iterator(of search result) is pointing at : " << *searchptr << endl;
             cout << "database before : ";
             database.Print();
             cout << endl;
             */
 
-            database.InsertBefore(positionfound,newdata);
+            database.InsertBefore(searchptr,newdata);
 
             /***DEBUG***
             cout << "database after insert: ";
             database.Print();
             cout << endl;
-            cout << "what search-iterator is poiting at: " << *positionfound << endl;
+            cout << "what search-iterator is poiting at: " << *searchptr << endl;
             */
 
-            database.Delete(positionfound);
+            database.Delete(searchptr);
 
             /***DEBUG***
             cout << "database after delete : ";
@@ -124,7 +132,7 @@ void set(list<data> &database, char* propertyname, char* newvalue)
             cout << endl;
 
             data tester;
-            tester = database.Delete(positionfound);
+            tester = database.Delete(searchptr);
             cout << "tester : " << tester << endl;
             database.Print();
             */
@@ -135,6 +143,20 @@ void set(list<data> &database, char* propertyname, char* newvalue)
     {
         database.Append(newdata);
     }
+}
+
+void get(list<data> database, char* propertyname)
+{
+    data dummydata;
+    dummydata.propertyname = propertyname;
+
+    Iterator<data> searchptr = database.Ithnode(database.Search(dummydata));
+    cout << "[" << *searchptr << "]";
+}
+
+void getall(list<data> database)
+{
+    database.Print();
 }
 
 int whattype(char* newvalue)
